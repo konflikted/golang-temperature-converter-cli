e1d6564f74ac597d75c3a0a4a7eb335bdc235abc
Eric <konflikted@gmail@users.noreply.github.com>
2020-09-02 22:04:29 -0400

    Still trying to get it to work.

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	}
 
 	originUnit = strings.ToUpper(os.Args[1])
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 		_, err = fmt.Scanln(&originValue)
